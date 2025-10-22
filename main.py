2d6d0f071d6cb8d13bcd77d97be7f37733369c0f
Vladimir Chizmejyan <harbecox@gmail.com>
2025-10-22 17:29:18 +0400

    test

---
+++ main.py
@@ -0,0 +1,73 @@
+import csv
+import gzip
+import io
+import json
+import re
+import time
+import brotli
+
+from seleniumwire import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+
+with open('urls.json','r',encoding='utf-8') as url_file:
+    search_urls = json.loads(url_file.read())
+    for index,url in enumerate(search_urls):
+        driver.get(url)
+        match = re.search(r'"pages"\s*:\s*(\d+)\s*,\s*"total"', driver.page_source)
+        pages = int(match.group(1))
+
+        urls = []
+        pattern = re.compile(r"^https://2gis\.ru/.+?/firm/.+")
+
+        for p in range(1,pages + 1):
+            print((index + 1),':',(len(search_urls) + 1),"\t",p,':',pages)
+            url = url.replace('?','/page/' + str(p) + '?')
+            driver.get(url)
+            aa = driver.find_elements(By.CSS_SELECTOR,'a')
+            for a in aa:
+                href = str(a.get_attribute('href'))
+                if href and pattern.match(href):
+                    urls.append(href.split("?")[0])
+
+        with open('p'+str(index)+'.json','w') as f:
+            f.write(json.dumps(urls,ensure_ascii=False))
+
+        # time.sleep(10000)
+        # input = driver.find_element(By.CSS_SELECTOR,'input[enterkeyhint="search"]')
+        # input.click()
+        # input.send_keys(Keys.ENTER)
+        # time.sleep(5)
+        # json_object = {}
+        # for request in driver.requests:
+        #     if request.response and request.url.startswith('https://catalog.api.2gis.ru/3.0/items?key'):
+        #         body_bytes = request.response.body
+        #         try:
+        #             decompressed_data = brotli.decompress(body_bytes)
+        #             json_string = decompressed_data.decode('utf-8')
+        #             json_object = json.loads(json_string)
+        #             print("Успешно полученный JSON:")
+        #             with open('output.json','w') as f:
+        #                 f.write(json_string)
+        #             for item in json_object.get('result').get('items'):
+        #                 print(item)
+        #         except brotli.error as e:
+        #             print(f"Ошибка декомпрессии Brotli. Возможно, данные повреждены или не являются Brotli: {e}")
+        #         except UnicodeDecodeError as e:
+        #             print(f"Ошибка декодирования UTF-8 после декомпрессии: {e}")
+        #         except json.JSONDecodeError as e:
+        #             print(f"Ошибка парсинга JSON: Декомпрессия и декодирование успешны, но содержимое не является корре")
+
+
+
+
+
+
+time.sleep(100000)
+
+
+
+
+
